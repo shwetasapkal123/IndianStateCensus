9a02466ef8860f7cbc0e8809d6b1356d3d8a2748
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-02-26 17:35:06 +0530

    [shweta] welcome msg

---
+++ IndianStateCensusAnalyser/IndianStateCensusAnalyser/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyser
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome");
+            Console.ReadLine();
+        }
+    }
+}
